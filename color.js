8cd63dfe172fcddbe585c8fabcf1e83e124e2714
Tom Trahan <tom@shippable.com>
2016-09-02 14:39:19 -0700

    green

--- color.js
+++ color.js
@@ -1,9 +1,9 @@
 module.exports = {
-  //color: '#038505'  //GREEN
+  color: '#038505'  //GREEN
   //color: '#1657b8'  //BLUE
   //color: '#980c8e'  //PURPLE
   //color: '#bf4912'  //RED
-  color: '#ac8601'  //ORANGE
+  //color: '#ac8601'  //ORANGE
   //color: '#449999'  //TEAL
   //color: '#FFFF00'  //YELLOW
 };
